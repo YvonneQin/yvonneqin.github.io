3e1286588db32c3517e5a3a5cda2c98f65a6ef4a
YvonneQin <tatamato811@gmail.com>
2025-08-27 13:18:42 +0800

    fix Skills

--- js/components/header.js
+++ js/components/header.js
@@ -16,6 +16,7 @@ class SiteHeader extends HTMLElement {
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-lg-auto">
           <li class="nav-item"><a class="nav-link" href="index.html">Homepage</a></li>
+          <li class="nav-item"><a data-scroll class="nav-link" href="index.html#skills">Skills</a></li>
           <li class="nav-item"><a data-scroll class="nav-link" href="index.html#portfolio">🧩 Tinkering Zone</a></li>
           <li class="nav-item"><a data-scroll class="nav-link" href="my_insight.html">Insights</a></li>
           <li class="nav-item"><a data-scroll class="nav-link" href="index.html#contact">Contact</a></li>
